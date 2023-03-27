6a0dabe7ef239a0a809eacd73a921c238fdaadde
blaizans <99187215+blaizans@users.noreply.github.com>
2023-03-27 12:12:58 +0200

    Blazorise icons

--- BlazorSyncTask/Program.cs
+++ BlazorSyncTask/Program.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorSyncTask;
@@ -7,5 +10,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services
+        .AddBlazorise( options =>
+        {
+            options.Immediate = true;
+        })
+        .AddBootstrapProviders()
+       .AddFontAwesomeIcons();
 await builder.Build().RunAsync();
