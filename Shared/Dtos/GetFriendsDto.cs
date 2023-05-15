962d95f17fc3b571edcb4fa974b6f0cb6ea72cbf
Jack Spinola <65230037+kokas340@users.noreply.github.com>
2023-05-15 17:45:33 +0200

    bug

---
+++ Shared/Dtos/GetFriendsDto.cs
@@ -0,0 +1,12 @@
+namespace Shared.Dtos;
+
+public class GetFriendsDto
+{
+    public int id { get; init; }
+    public string fullName { get; init; }
+    public string username { get; init; }
+    public string email { get; init; }
+    public int friendRequstId { get; init; }
+    
+    
+}
